ebba111b2a8dab2822237ea94b35e1207f7a9249
venegretik <68019238+venegretik@users.noreply.github.com>
2021-01-20 14:57:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(".loupe").click(function () {
+$(".full-block").addClass("display-block").removeClass("display-none");
+
+$(this).addClass("active");
+$(this).removeClass("loupe");
+$(".active").click(function () {
+$(".full-block").removeClass("display-block").addClass("display-none");
+});
+
+});
